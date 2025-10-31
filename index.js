8cb38e653b0af2dd0e305959e7af2f32fac5e162
Damirland <dmuratov755@gmail.com>
2025-10-31 18:12:45 +0400

    changes

--- index.js
+++ index.js
@@ -2,12 +2,43 @@ let board = document.querySelector(".board")
 let start_button = document.querySelector(".board__button")
 let input = document.querySelector(".board__input")
 
-function createBoard() {
-    board.replaceChildren()
+function createBoard(columns, count) {
+    board.textContent = ""
+
+    const template = document.querySelector('#gameTableTemplate')
+    const gameTable = template.querySelector(".table")
+    const restartBtn = gameTable.querySelector(".table__button");
+    
+    // gameTable.style = `
+    //     grid-template-columns: repeat(${columns}, 1fr)
+    //     grid-template-rows: repeat(${columns}, 1fr)
+    // `
+
+    gameTable.style.gridTemplateCollumns = `repeat(${columns}, 1fr)`
+    gameTable.style.gridTemplateRows = `repeat(${columns}, 1fr)`
+    
+    for (let i = 0; i < count; i++) {
+        gameTable.append(createCard())
+    }
+
+    board.append(gameTable)
+    
+    restartBtn.addEventListener("click", ()=>{
+        location.reload
+    })
+
+    board.append(restartBtn)
+
+}
+
+function createCard() {
+    let template = document.querySelector("#cardTemplate")
+    let card = template.querySelector(".card")
+    
+
 }
 
 start_button.addEventListener("click", ()=>{
-    console.log(input.value)
     if (input.value % 2 === 0)  {
         input.value = input.value * input.value
     } else {
